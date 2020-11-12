30afc1bebb2f29fd44867c3eb1ea494fa2fb5f70
Code4Blessings <robincw35@gmail.com>
2020-11-12 13:26:34 -0500

    Added necessary dependencies

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require('cors');
+const helmet = require('helmet');
+const server = express();
+
+const userRouter = require("../users/users-router")
+const authRouter = require("../auth/auth-router");
+const postRouter = require("../posts/post-router");
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use("/api/auth", authRouter);
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+
+server.get("/", (req, res) => {
+  res.send({ Greeting: 'Greetings from Port 5500!!!' });
+});
+
+module.exports = server;
