36fabc22e159588dcfbcb8b1399dc742ee646139
Code4Blessings <robincw35@gmail.com>
2020-11-12 15:40:58 -0500

    Changed location of port variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require("./api/server");
 
-const port = process.env.Port;
+const port = process.env.Port || 5000
 
 server.listen(port, () =>
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`)
