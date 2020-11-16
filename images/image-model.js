fb92e9963fb3663cefc0e42496ea0ebc7526b3b7
Code4Blessings <robincw35@gmail.com>
2020-11-16 14:30:06 -0500

    Images and posts table caused infinite loop. Need to fix

--- images/image-model.js
+++ images/image-model.js
@@ -1,38 +1,38 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
-const db = knex(knexConfig.development);
+// const knex = require('knex');
+// const knexConfig = require('../knexfile.js');
+// const db = knex(knexConfig.development);
 
-module.exports = {
-  find,
-  findById,
-  insert,
-  update,
-  remove
-};
+// module.exports = {
+//   find,
+//   findById,
+//   insert,
+//   update,
+//   remove
+// };
 
-function find() {
-  return db('images');
-}
+// function find() {
+//   return db('images');
+// }
 
-function findById(id) {
-  return db('images').where({ id: Number(id) });
-}
+// function findById(id) {
+//   return db('images').where({ id: Number(id) });
+// }
 
-function insert(image) {
-  return db('images')
-    .insert(post, 'id')
-    .then(ids => ({ id: ids[0] }));
-}
+// function insert(image) {
+//   return db('images')
+//     .insert(post, 'id')
+//     .then(ids => ({ id: ids[0] }));
+// }
 
-function update(id, image) {
-  return db('images')
-    .where('id', Number(id))
-    .update(image);
-}
+// function update(id, image) {
+//   return db('images')
+//     .where('id', Number(id))
+//     .update(image);
+// }
 
-function remove(id) {
-  return db('images')
-    .where('id', Number(id))
-    .del();
-}
+// function remove(id) {
+//   return db('images')
+//     .where('id', Number(id))
+//     .del();
+// }
 
--- images/image-router.js
+++ images/image-router.js
@@ -1,59 +1,55 @@
-const express = require('express');
-const router = express.Router();
-const dB = require('../data/dbConfig');
-const Images = require('./image-model');
+// const express = require('express');
+// const router = express.Router();
+// const dB = require('../data/dbConfig');
+// const Images = require('./image-model');
 
 
 
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
  
- Users.find()
-    .then(images => {
-        res.status(200).json(images)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ 
-            errorMessage: "Error retrieving the list of images", 
-            message: err.message
-        })
-    })
-})
+//  Users.find()
+//     .then(images => {
+//         res.status(200).json(images)
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json({ 
+//             errorMessage: "Error retrieving the list of images", 
+//             message: err.message
+//         })
+//     })
+// })
 
-router.put('/:id', (req, res) => {
-      const id = req.params.id;
-      const imageData = req.body; 
-      console.log(id);
-      console.log(imageData);
-      Users.findById(id)
-      .then(image => {
-          console.log(image.id);
-          if(image.id == id) {
-              if(imageData.password){
-                  const hash = bcrypt.hashSync(userData.password, 8);
-                  userData.password = hash;
-              }
-              Users.update(id, userData)
-              .then(updatedUser => {
-                  res.status(201).json({message: 'User updated successfully!!!'})
-              })
-              .catch(err => {
-                console.log(err)
-                res.status(404).json({ 
-                    message: 'User not found',
-                    errorMessage: err.message
-            })
-              })
-          }
-      })
-      .catch(err => {
-          res.status(500).json({
-               message: "User could not be updated",
-               errorMessage: err.message
-            })
-      })
- })
+// router.put('/:id', (req, res) => {
+//       const id = req.params.id;
+//       const imageData = req.body; 
+//       console.log(id);
+//       console.log(imageData);
+//       Images.findById(id)
+//       .then(image => {
+//           console.log(image.id);
+//           if(image.id == id) {
+//               Images.update(id, userData)
+//               .then(updatedImage => {
+//                   res.status(201).json({message: 'Image updated successfully!!!'})
+//               })
+//               .catch(err => {
+//                 console.log(err)
+//                 res.status(404).json({ 
+//                     message: 'Image not found',
+//                     errorMessage: err.message
+//             })
+//               })
+//           }
+//       })
+//       .catch(err => {
+//           res.status(500).json({
+//                message: "User could not be updated",
+//                errorMessage: err.message
+//             })
+//       })
+//  })
 
 
 
-module.exports = router;
+// module.exports = router;
